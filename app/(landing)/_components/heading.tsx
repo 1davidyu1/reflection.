5d0646c149f3ca5a2eed5b85791ab9ec67f84006
1davidyu1 <yudavid@msu.edu>
2024-04-25 15:27:59 -0400

    basing landing skeleton

---
+++ app/(landing)/_components/heading.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export const Heading = () => {
+    return ( 
+        <div className="text-black">
+            Heading
+        </div>
+     );
+}
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,15 @@
+import { Footer } from "./_components/footer";
+import { Heading } from "./_components/heading";
+import { Heroes } from "./_components/heroes";
+
 const Landing = () => {
     return ( 
-        <div>
-            Main
+        <div className="min-h-full flex flex-col">
+            <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+                <Heading />
+                <Heroes />
+            </div>
+            <Footer />
         </div>
      );
 }
